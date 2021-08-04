6a4283ab7009df94cc4eb3e51e903446601a42b7
Alex Nyika Omuyonga <nyika@medicmobile.org>
2021-08-04 11:29:28 +0300

    Set up the stable  Docker repository
    Add command to set up the stable  Docker repository

--- self-hosting/prepare-system/ubuntu/prepare.sh
+++ self-hosting/prepare-system/ubuntu/prepare.sh
@@ -9,6 +9,9 @@ sudo mkdir -p /var/log/medic
 sudo apt-get update
 
 # Install Docker CE
+sudo echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo apt install -y docker.io
 
